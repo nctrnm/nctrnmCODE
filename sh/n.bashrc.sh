3e5a8fcb61f3e6cade8ccbc3d9ec8579d1a4824b
nctrnm <48932877+nctrnm@users.noreply.github.com>
2024-09-22 13:14:42 -0700

    u: 2024-09-22 13:14:42 Updated files: sh/n.bashrc.sh

--- sh/n.bashrc.sh
+++ sh/n.bashrc.sh
@@ -14,7 +14,7 @@
 alias bed='nano ~/.bashrc && source ~/.bashrc && ls -l /sdcard/Documents > /sdcard/Documents/aliases_$(date +"%m%d%Y").txt && cp ~/.bashrc ~/.bash_profile && cp ~/.bashrc /sdcard/Documents/code/sh/n.bashrc.sh && bash' # Edit and reload .bashrc, create directory text file, copy to repository
 alias i='apt-get install' # Install packages
 alias c='clear' # Clear the terminal screen
-alias gem='termux-open https://gemini.google.com' #Opem Google Gemininin web browser
+alias gem='termux-open https://gemini.google.com' #Open Google Geminin in web browser
 alias h='history' # Show command history
 alias py='python' # Run python
 alias la='ls -a' # List all files including hidden
