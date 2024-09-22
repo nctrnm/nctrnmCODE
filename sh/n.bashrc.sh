b973b95308e5d0ee6747ac5da78364fbc343a122
nctrnm <48932877+nctrnm@users.noreply.github.com>
2024-09-22 12:23:29 -0700

    u: 2024-09-22 12:23:29 [about message]

--- sh/n.bashrc.sh
+++ sh/n.bashrc.sh
@@ -14,6 +14,7 @@
 alias bed='nano ~/.bashrc && source ~/.bashrc && ls -l /sdcard/Documents > /sdcard/Documents/aliases_$(date +"%m%d%Y").txt && cp ~/.bashrc ~/.bash_profile && cp ~/.bashrc /sdcard/Documents/code/sh/n.bashrc.sh' # Edit and reload .bashrc, create directory text file, copy to repository
 alias i='apt-get install' # Install packages
 alias c='clear' # Clear the terminal screen
+alias gem='termux-open https://gemini.google.com'
 alias h='history' # Show command history
 alias py='python'
 alias la='ls -a' # List all files including hidden
